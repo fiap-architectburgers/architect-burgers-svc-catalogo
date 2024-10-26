bcf99f37db7ca2556965bd0cea325d55dd419763
Rodrigo Gomes <rodrigo.gomes@viasat.com>
2024-10-26 17:27:15 -0300

    Restore unit tests

--- modules/entities/src/test/java/com/example/gomesrodris/archburgers/domain/entities/ItemCardapioTest.java
+++ modules/entities/src/test/java/com/example/gomesrodris/archburgers/domain/entities/ItemCardapioTest.java
@@ -13,30 +13,30 @@ class ItemCardapioTest {
 
     private ItemCardapio itemCardapio;
 
-//    @BeforeEach
-//    void setUp() {
-//        itemCardapio = new ItemCardapio(21, TipoItemCardapio.LANCHE, "Hamburger Vegetariano",
-//                "Hamburger de ervilha com queijo vegano",
-//                new ValorMonetario("23.50"));
-//    }
-//
-//    @Test
-//    void checkAttributes() {
-//        assertThat(itemCardapio.id()).isEqualTo(21);
-//        assertThat(itemCardapio.tipo()).isEqualTo(TipoItemCardapio.LANCHE);
-//        assertThat(itemCardapio.nome()).isEqualTo("Hamburger Vegetariano");
-//        assertThat(itemCardapio.descricao()).isEqualTo("Hamburger de ervilha com queijo vegano");
-//        assertThat(itemCardapio.valor()).isEqualTo(new ValorMonetario(new BigDecimal("23.50")));
-//    }
-//
-//    @Test
-//    void withId() {
-//        var newItem = itemCardapio.withId(45);
-//        assertThat(newItem).isNotSameAs(itemCardapio);
-//        assertThat(newItem.id()).isEqualTo(45);
-//        assertThat(newItem.tipo()).isEqualTo(itemCardapio.tipo());
-//        assertThat(newItem.nome()).isEqualTo(itemCardapio.nome());
-//        assertThat(newItem.descricao()).isEqualTo(itemCardapio.descricao());
-//        assertThat(newItem.valor()).isEqualTo(itemCardapio.valor());
-//    }
+    @BeforeEach
+    void setUp() {
+        itemCardapio = new ItemCardapio(21, TipoItemCardapio.LANCHE, "Hamburger Vegetariano",
+                "Hamburger de ervilha com queijo vegano",
+                new ValorMonetario("23.50"));
+    }
+
+    @Test
+    void checkAttributes() {
+        assertThat(itemCardapio.id()).isEqualTo(21);
+        assertThat(itemCardapio.tipo()).isEqualTo(TipoItemCardapio.LANCHE);
+        assertThat(itemCardapio.nome()).isEqualTo("Hamburger Vegetariano");
+        assertThat(itemCardapio.descricao()).isEqualTo("Hamburger de ervilha com queijo vegano");
+        assertThat(itemCardapio.valor()).isEqualTo(new ValorMonetario(new BigDecimal("23.50")));
+    }
+
+    @Test
+    void withId() {
+        var newItem = itemCardapio.withId(45);
+        assertThat(newItem).isNotSameAs(itemCardapio);
+        assertThat(newItem.id()).isEqualTo(45);
+        assertThat(newItem.tipo()).isEqualTo(itemCardapio.tipo());
+        assertThat(newItem.nome()).isEqualTo(itemCardapio.nome());
+        assertThat(newItem.descricao()).isEqualTo(itemCardapio.descricao());
+        assertThat(newItem.valor()).isEqualTo(itemCardapio.valor());
+    }
 }
--- modules/entities/src/test/java/com/example/gomesrodris/archburgers/domain/valueobjects/TipoItemCardapioTest.java
+++ modules/entities/src/test/java/com/example/gomesrodris/archburgers/domain/valueobjects/TipoItemCardapioTest.java
@@ -7,24 +7,24 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class TipoItemCardapioTest {
 
-//    @Test
-//    void getByAbreviacaoLancheTest() {
-//        assertEquals(TipoItemCardapio.LANCHE, TipoItemCardapio.getByAbreviacao("L"));
-//    }
-//
-//    @Test
-//    void getByAbreviacaoTest() {
-//        assertEquals(TipoItemCardapio.ACOMPANHAMENTO, TipoItemCardapio.getByAbreviacao("A"));
-//        assertEquals(TipoItemCardapio.BEBIDA, TipoItemCardapio.getByAbreviacao("B"));
-//        assertEquals(TipoItemCardapio.SOBREMESA, TipoItemCardapio.getByAbreviacao("S"));
-//        assertThrows(IllegalArgumentException.class, () -> TipoItemCardapio.getByAbreviacao("Z"));
-//    }
-//
-//    @Test
-//    void getAbreviacaoTest() {
-//        assertEquals("L", TipoItemCardapio.LANCHE.getAbreviacao());
-//        assertEquals("A", TipoItemCardapio.ACOMPANHAMENTO.getAbreviacao());
-//        assertEquals("B", TipoItemCardapio.BEBIDA.getAbreviacao());
-//        assertEquals("S", TipoItemCardapio.SOBREMESA.getAbreviacao());
-//    }
+    @Test
+    void getByAbreviacaoLancheTest() {
+        assertEquals(TipoItemCardapio.LANCHE, TipoItemCardapio.getByAbreviacao("L"));
+    }
+
+    @Test
+    void getByAbreviacaoTest() {
+        assertEquals(TipoItemCardapio.ACOMPANHAMENTO, TipoItemCardapio.getByAbreviacao("A"));
+        assertEquals(TipoItemCardapio.BEBIDA, TipoItemCardapio.getByAbreviacao("B"));
+        assertEquals(TipoItemCardapio.SOBREMESA, TipoItemCardapio.getByAbreviacao("S"));
+        assertThrows(IllegalArgumentException.class, () -> TipoItemCardapio.getByAbreviacao("Z"));
+    }
+
+    @Test
+    void getAbreviacaoTest() {
+        assertEquals("L", TipoItemCardapio.LANCHE.getAbreviacao());
+        assertEquals("A", TipoItemCardapio.ACOMPANHAMENTO.getAbreviacao());
+        assertEquals("B", TipoItemCardapio.BEBIDA.getAbreviacao());
+        assertEquals("S", TipoItemCardapio.SOBREMESA.getAbreviacao());
+    }
 }
